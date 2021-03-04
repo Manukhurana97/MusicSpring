0f0a9bd23a6dcd5c93617d4cf7092dce00a6cdfe
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2021-03-04 20:20:03 +0530

    Update EmailConfig.java

--- Security-userservice/src/main/java/com/example/demo/Email/EmailConfig.java
+++ Security-userservice/src/main/java/com/example/demo/Email/EmailConfig.java
@@ -16,8 +16,8 @@ public class EmailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("indiastrength.info@gmail.com");
-        mailSender.setPassword("indiastrengthinfo@123");
+        mailSender.setUsername("email");
+        mailSender.setPassword("password");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
